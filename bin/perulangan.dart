74b5b5916d4d2d14d99b5b861945d6721caf5b86
Susi <susimardalena80@gmail.com>
2024-10-17 13:41:59 +0800

    operator

---
+++ bin/perulangan.dart
@@ -0,0 +1,16 @@
+void main() {
+  int counter = 0;
+
+  // A simple while loop
+  while (counter < 5) {
+    print('Counter: $counter');
+    counter++;
+  }
+
+  // Using a do-while loop
+  int num = 0;
+  do {
+    print('Number: $num');
+    num++;
+  } while (num < 3);
+}
