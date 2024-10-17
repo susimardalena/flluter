45d45f5cbe3bd2779794a92db0b5f3c46399e22b
Susi <susimardalena80@gmail.com>
2024-10-17 14:00:18 +0800

    operator

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,22 @@
+void main() {
+  int x = 10;
+  int y = 20;
+
+  // Operator lebih besar
+  print('x > y: ${x > y}');  // false
+
+  // Operator lebih kecil
+  print('x < y: ${x < y}');  // true
+
+  // Operator lebih besar atau sama dengan
+  print('x >= y: ${x >= y}');  // false
+
+  // Operator lebih kecil atau sama dengan
+  print('x <= y: ${x <= y}');  // true
+
+  // Operator sama dengan
+  print('x == y: ${x == y}');  // false
+
+  // Operator tidak sama dengan
+  print('x != y: ${x != y}');  // true
+}
