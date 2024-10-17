c4a7c7d65f2c8f6e6cbcd609341fab67a6a7f588
Susi <susimardalena80@gmail.com>
2024-10-17 13:41:04 +0800

    komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,14 @@
+// Ini adalah komentar satu baris
+
+/*
+Ini adalah komentar banyak baris
+Anda dapat menulis banyak baris seperti yang anda inginkan
+*/
+
+/// Ini adalah komentar dokumentasi
+/// Anda dapat menggunakan untuk menjalakan fungsi atau kelas
+
+void main(){
+  //Anda juga dapat menggunakan komentar di dalam fungsi
+  print ('Halo, Dunia');
+}
